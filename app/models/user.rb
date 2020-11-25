3ede5083b2d87d30ded2df72cd874bce32028b91
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-25 14:17:07 +0900

    サインアップ時の追加バリデーション
    エラーメッセージも表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,message: 'Full-width characters'}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,message: 'Full-width characters'}
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: 'Full-width katakana characters'}
+  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: 'Full-width katakana characters'}
   validates :birth_day, presence: true
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'Include both letters and numbers'}
 end
