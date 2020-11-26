017e9a5c8dc55a741c54854d0f4d8adbc061f2b1
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-26 18:09:29 +0900

    Rubocopの実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :birth_day, :first_name, :family_name, :first_name_kana, :family_name_kana])
+      :sign_up, keys: [:nickname, :birth_day, :first_name, :family_name, :first_name_kana, :family_name_kana]
+    )
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,message: 'Full-width characters'}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   validates :birth_day, presence: true
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 end
