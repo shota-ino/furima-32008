fbfa057e4856d3cb64d5597fd882d17560e3b00c
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-26 17:56:45 +0900

    モデルの単体テストコード
    一部、バリデーション変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {"1234ab"}
+    password              {"1234Ab"}
     password_confirmation {password}
     first_name            {Gimei.first.kanji}
     family_name           {Gimei.last.kanji}
