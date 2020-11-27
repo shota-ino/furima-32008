4b6ea8cb692f5f866fefc74c8b17c65f47974538
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-27 16:17:29 +0900

    商品出品ページを表示&ActiveHashの中身設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      # 成功した場合はredirect_toメソッドでルートパスにリダイレクト
+      redirect_to root_path
+    else
+      # 失敗した場合はrenderメソッドでprototypes/new.html.erbのページを表示
+      render :new
+    end
+  end
+
 end
