e895c7501cba3e8fbeb78b3d26d520ab748107fa
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-27 11:59:27 +0900

    ActiveHashの導入
    5つ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,19 @@
 class Item < ApplicationRecord
   
-  # validates :, presence: true
-  # validates :, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :prefecture_code
+  belongs_to :preparation_day
+
+  validates :name, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :postage_payer_id, numericality: { other_than: 1 }
+  validates :prefecture_code_id, numericality: { other_than: 1 }
+  validates :preparation_day_id, numericality: { other_than: 1 }
+  validates :price, presence: true
   # validates :, presence: true
   # validates :, presence: true
   validates :image, presence: true
@@ -9,4 +21,6 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one    :order
   has_one_attached :image
+
+
 end
