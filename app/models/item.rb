31eb190585615d1a66b20897157d3b5b5a3cf134
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-27 11:35:31 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-
-  # has_one_attached :image
   
   # validates :, presence: true
   # validates :, presence: true
   # validates :, presence: true
   # validates :, presence: true
+  validates :image, presence: true
 
   belongs_to :user
   # has_one    :order
+  has_one_attached :image
 end
