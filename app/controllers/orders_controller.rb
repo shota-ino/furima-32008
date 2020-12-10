6238c8286d5eef826fa04751603b5172467abbca
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-12-10 19:09:26 +0900

    不要な記述＆ファイルの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:index, :create, :sold_out]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_destination = OrderDestination.new
