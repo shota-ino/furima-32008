fe30e0c720b74ac0f32a83cf477c295bc73e010e
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-12-10 16:48:13 +0900

    sold_outの表示、不正アクセスのトップへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: :edit
+  before_action :get_item, only: :edit
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -56,6 +57,14 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def get_item
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :my_items
-  before_action :sold_out
-  before_action :set_item, only: [:index, :create]
+  before_action :set_item, only: [:index, :create, :sold_out]
 
   def index
     @order_destination = OrderDestination.new
+    if current_user.id == @item.user_id || @item.order != nil
+      redirect_to root_path 
+    end
   end
 
   def create
@@ -15,21 +16,11 @@ class OrdersController < ApplicationController
       @order_destination.save
       redirect_to root_path
     else
-      render 'index'
-      @order_destination = OrderDestination.new
+      render :index
     end
   end
 
   private
-
-  def my_items
-    @item = Item.find(params[:item_id])
-    redirect_to root_path unless @item.user != current_user
-  end
-
-  def sold_out
-    redirect_to root_path if @item.order.present?
-  end
 
   def set_item
     @item = Item.find(params[:item_id])
